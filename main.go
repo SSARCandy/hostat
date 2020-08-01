2cbfeb180cc24e25e46a97f18073a595d0aa4ed9
SSARCandy <ssarcandy@gmail.com>
2020-08-01 16:50:13 +0800

    try

--- main.go
+++ main.go
@@ -5,20 +5,21 @@ import (
 
   "github.com/fatih/color"
   "github.com/shirou/gopsutil/mem"
+  "github.com/shirou/gopsutil/load"
 )
 
+/**
+ * hostname  | CPUs | 1m Load | 5m Load | 15m Load | memory % | disk % | users | uptime | status | jobs
+ * cluster01 |    8 |     1.1 |     1.5 |      2.1 |     60 % |   56 % |     2 | 16days |  alloc | ssarcandy(8)
+ * cluster02 |    8 |     5.1 |     5.5 |      5.1 |     20 % |   96 % |     0 | 16days |  alloc | ssarcandy(8)
+ */
+
 func main() {
-  v, _ := mem.VirtualMemory()
+  red := color.New(color.FgRed).SprintFunc()
+  m, _ := mem.VirtualMemory()
+  l, _ := load.Avg()
 
-  // almost every return value is a struct
-  fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", v.Total, v.Free, v.UsedPercent)
-
-  // convert to JSON. String() is also implemented
-  fmt.Println(v)
-  // Print with default helper functions
-  color.Cyan("Prints text in cyan.")
-
-  // A newline will be appended automatically
-  color.Blue("Prints %s in blue.", "text")
+  fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", red(m.Total), m.Free, m.UsedPercent)
+  fmt.Printf("1m: %v, 5m: %v, 15m: %v\n", l.Load1, l.Load5, l.Load15)
 }
 
