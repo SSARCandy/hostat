9344f5412b2d2cc52512b23946c8e9f95c2cecb4
SSARCandy <ssarcandy@gmail.com>
2020-08-01 16:50:13 +0800

    try

--- main.go
+++ main.go
@@ -3,11 +3,9 @@ package main
 import (
 	"fmt"
 
-	"github.com/shirou/gopsutil/cpu"
-	"github.com/shirou/gopsutil/load"
-	"github.com/shirou/gopsutil/mem"
-	"github.com/shirou/gopsutil/host"
-	"github.com/shirou/gopsutil/disk"
+  "github.com/fatih/color"
+  "github.com/shirou/gopsutil/mem"
+  "github.com/shirou/gopsutil/load"
 )
 
 /**
@@ -17,19 +15,10 @@ import (
  */
 
 func main() {
-	// red := color.New(color.FgRed).SprintFunc()
-	m, _ := mem.VirtualMemory()
-	l, _ := load.Avg()
-	c, _ := cpu.Counts(true)
-	i, _ := host.Info()
-	d, _ := disk.Usage("/")
+  red := color.New(color.FgRed).SprintFunc()
+  m, _ := mem.VirtualMemory()
+  l, _ := load.Avg()
 
-	// print header
-	fmt.Printf("%-10s |%5s |%7s |%7s |%7s |%8s |%6s |\n", "hostname", "CPUs", "1m", "5m", "15m", "memory%", "disk%")
-
-	fmt.Printf("%-10s |", i.Hostname)
-	fmt.Printf("%5v |", c)
-	fmt.Printf("%7.1f |%7.1f |%7.1f |", l.Load1, l.Load5, l.Load15)
-	fmt.Printf("%7.1f%% |", m.UsedPercent)
-	fmt.Printf("%5.1f%% |\n", d.UsedPercent)
+  fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", red(m.Total), m.Free, m.UsedPercent)
+  fmt.Printf("1m: %v, 5m: %v, 15m: %v\n", l.Load1, l.Load5, l.Load15)
 }
