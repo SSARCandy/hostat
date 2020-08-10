92c2750b747473ed599bd2ee06b9e666f9f55484
Andy Hsu <ssarcandy@gmail.com>
2020-08-10 11:54:09 +0800

    poc

--- main.go
+++ main.go
@@ -1,24 +1,35 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 
-  "github.com/fatih/color"
-  "github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/load"
+	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/disk"
 )
 
+/**
+ * hostname  | CPUs | 1m Load | 5m Load | 15m Load | memory % | disk % | users | uptime | status | jobs
+ * cluster01 |    8 |     1.1 |     1.5 |      2.1 |     60 % |   56 % |     2 | 16days |  alloc | ssarcandy(8)
+ * cluster02 |    8 |     5.1 |     5.5 |      5.1 |     20 % |   96 % |     0 | 16days |  alloc | ssarcandy(8)
+ */
+
 func main() {
-  v, _ := mem.VirtualMemory()
+	// red := color.New(color.FgRed).SprintFunc()
+	m, _ := mem.VirtualMemory()
+	l, _ := load.Avg()
+	c, _ := cpu.Counts(true)
+	i, _ := host.Info()
+	d, _ := disk.Usage("/")
 
-  // almost every return value is a struct
-  fmt.Printf("Total: %v, Free:%v, UsedPercent:%f%%\n", v.Total, v.Free, v.UsedPercent)
+	// print header
+	fmt.Printf("%-10s |%5s |%7s |%7s |%7s |%8s |%6s |\n", "hostname", "CPUs", "1m", "5m", "15m", "memory%", "disk%")
 
-  // convert to JSON. String() is also implemented
-  fmt.Println(v)
-  // Print with default helper functions
-  color.Cyan("Prints text in cyan.")
-
-  // A newline will be appended automatically
-  color.Blue("Prints %s in blue.", "text")
+	fmt.Printf("%-10s |", i.Hostname)
+	fmt.Printf("%5v |", c)
+	fmt.Printf("%7.1f |%7.1f |%7.1f |", l.Load1, l.Load5, l.Load15)
+	fmt.Printf("%7.1f%% |", m.UsedPercent)
+	fmt.Printf("%5.1f%% |\n", d.UsedPercent)
 }
-
