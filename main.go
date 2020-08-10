fe914cd97d7aabdee3039dafd4293708079c275a
ssarcandy <ssarcandy@gmail.com>
2020-08-10 03:53:07 +0000

    add disk

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/load"
 	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/host"
+	"github.com/shirou/gopsutil/disk"
 )
 
 /**
@@ -19,12 +21,15 @@ func main() {
 	m, _ := mem.VirtualMemory()
 	l, _ := load.Avg()
 	c, _ := cpu.Counts(true)
+	i, _ := host.Info()
+	d, _ := disk.Usage("/")
 
 	// print header
-	fmt.Printf("%-10s |%5s |%7s |%7s |%7s |%8s |\n", "hostname", "CPUs", "1m", "5m", "15m", "memory%")
+	fmt.Printf("%-10s |%5s |%7s |%7s |%7s |%8s |%6s |\n", "hostname", "CPUs", "1m", "5m", "15m", "memory%", "disk%")
 
-	fmt.Printf("%-10s |", "ssarcandy")
+	fmt.Printf("%-10s |", i.Hostname)
 	fmt.Printf("%5v |", c)
 	fmt.Printf("%7.1f |%7.1f |%7.1f |", l.Load1, l.Load5, l.Load15)
-	fmt.Printf("%7.1f%% |\n", m.UsedPercent)
+	fmt.Printf("%7.1f%% |", m.UsedPercent)
+	fmt.Printf("%5.1f%% |\n", d.UsedPercent)
 }
